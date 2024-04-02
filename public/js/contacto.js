516aeb924187d06ee9d69c2d9680abd7a7ff0b54
Chris0925-coder <christianguardia@outlook.es>
2024-04-02 11:52:46 -0500

    04022024

---
+++ public/js/contacto.js
@@ -0,0 +1,15 @@
+const navBoton = document.querySelector(".nav__menu");
+const navLink = document.querySelector(".nav__link");
+const navBotonClose = document.querySelector('.nav__close');
+
+navBotonClose.addEventListener('click', () => {
+	navLink.classList.toggle("nav__link--menu");
+});
+
+navBoton.addEventListener('click', () => {
+	navLink.classList.toggle("nav__link--menu");
+});
+
+let date = document.querySelector('.date');
+
+date.append(new Date ().getFullYear ());
