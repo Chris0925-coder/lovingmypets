86ceea50424d3cce0375838ce053206f9beec3ab
Chris0925-coder <christianguardia@outlook.es>
2026-04-20 23:06:16 -0500

    0

--- public/js/blog.js
+++ public/js/blog.js
@@ -73,3 +73,28 @@ newDiv.addEventListener('click', (e) => {
   }
 })
 
+const head = document.getElementsByTagName('title');
+const newBlog = document.getElementById('new-blog');
+const item2 = newBlog.querySelector('.item:nth-child(2)');
+let newDivBlog = document.createElement('main');
+
+function newPageContent(title, filename, paragraph,link,id) {
+  let l = link.split('/');
+    // console.log(l);
+    if(l[0] != "https:") link = `https://${link}`;
+
+  head.title.innerHTML = title;
+  newDiv.innerHTML = `
+      <section>
+        <h1>${title}</h1>
+        <figure>
+          <img src='https://qjsvnfogbaqnjbqi.public.blob.vercel-storage.com/lovingmypets/${filename}' alt='${title}'>
+            <figcaption>
+              <p><a href="${link}" title="${title}" target="_blank">${paragraph}</a></p>
+            </figcaption>
+        </figure>
+      </section>   
+  `;
+
+  newBlog.insertBefore(newDivBlog, item2);
+}
