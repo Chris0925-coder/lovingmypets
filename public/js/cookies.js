a47cce73237fd0f90279836d589e6072645047c4
Chris0925-coder <christianguardia@outlook.es>
2026-04-21 08:26:27 -0500

    00

---
+++ public/js/cookies.js
@@ -0,0 +1,46 @@
+function setCookie(cname, cvalue, exdays) {
+        let d = new Date();
+
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    };
+
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+
+function removeCookie(cname){
+    getCookie(cname,"",-1);
+};
+
+
+function detectCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0 && (name.length != c.length))  {
+            return true;
+        }
+    }
+    return false;
+};
---
+++ public/js/newBlog.js
@@ -0,0 +1,56 @@
+const head = document.getElementsByTagName('title');
+const newBlog = document.getElementById('new-blog');
+const item2 = newBlog.querySelector('.item:nth-child(2)');
+let newDivBlog = document.createElement('main');
+
+let article = 0;
+
+article = getCookie("article");
+
+console.log(article);
+
+async function getItem(value) {
+  let result = await fetch(`${urlGetItem}/${article}`, {
+      method: "GET",
+      // headers: {
+      //   Authorization: `Bearer ${token}`,
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
+      // },
+    })
+    .then((resp) => resp.json())
+    .catch((error) => {
+          console.error("Error:", error.message);
+          blog.style.color = "#990000";
+          blog.innerText = error.message;
+        });
+
+    // console.log(result.rows[0]);
+// rows[0]rows[0][0]rows[0]
+    // result.forEach((rows) => {
+      newPageContent(result.rows[0][1],result.rows[0][2],result.rows[0][3],result.rows[0][4],result.rows[0][0]);
+    // })
+    
+}
+
+function newPageContent(title, filename, paragraph,link,id) {
+  let l = link.split('/');
+    // console.log(l);
+    if(l[0] != "https:") link = `https://${link}`;
+
+  head[0].innerHTML = title;
+
+  newDivBlog.innerHTML = `
+      <section>
+        <h1>${title}</h1>
+        <figure>
+          <img src='https://qjsvnfogbaqnjbqi.public.blob.vercel-storage.com/lovingmypets/${filename}' alt='${title}'>
+            <figcaption>
+              <p><a href="${link}" title="${title}" target="_blank">${paragraph}</a></p>
+            </figcaption>
+        </figure>
+      </section>   
+  `;
+
+  newBlog.insertBefore(newDivBlog, item2);
+}
